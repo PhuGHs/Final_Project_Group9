d82910a5fd56a2772d5f24399d6a61cf495c83f3
Phu Le <21522466@gm.uit.edu.vn>
2022-10-28 20:51:07 +0700

    add filter function and add order page

--- RestaurantManagement/ViewModel/MenuViewModel.cs
+++ RestaurantManagement/ViewModel/MenuViewModel.cs
@@ -4,36 +4,106 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestaurantManagement.View;
 using System.Windows.Input;
 using Menu.Models;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Documents;
+using System.Printing;
 
 namespace QuanLyNhaHang.ViewModel
 {
     public class MenuViewModel : BaseViewModel
     {
         public MenuViewModel()
-
         {
             DirectingFoodToOrderListCommand = new RelayCommand<MenuItem>((p) => true, (p) => OrderAnItem(p.ID));
             RemovingFoodFromOrderListCommand = new RelayCommand<MenuItem>((p) => true, (p) => RemoveAnItem(p.ID));
-            
+            RemovingAllItemFromOrderList = new RelayCommand<object>((p) => true, (p) =>
+            {
+                MessageBoxResult result= System.Windows.MessageBox.Show("Bạn có muốn xoá tất cả các món đã đặt?", "Delete request", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                switch(result)
+                {
+                    case MessageBoxResult.Yes:
+                        RemoveAllItemOL();
+                        break;
+                    case MessageBoxResult.No:
+                        break; 
+                }
+            }) ;
+            OpeningDetailOrder = new RelayCommand<object>((p) => true, (p) =>
+            {
+                Order OrderWindow = new Order();               
+                OrderWindow.ShowDialog();
+            });
+            ShowPrintingDialog = new RelayCommand<object>((p) => true, (p) =>
+            {            
+                PrintPreviewDialog ppd = new PrintPreviewDialog();
+                
+                ppd.ShowDialog();
+            });
+            FoodCookingInform = new RelayCommand<object>((p) => true, (p) =>
+            {
+                System.Windows.MessageBox.Show("Báo chế biến thành công");
+            });
+            FilterFoodByPriceCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                if (MyComboboxSelectedItem_2 == "DESC Price")
+                {
+                    MenuItems = new ObservableCollection<MenuItem>(MenuItems.OrderBy(i => i.Price));
+                }
+                else if (MyComboboxSelectedItem_2 == "A-Z")
+                {
+                    MenuItems = new ObservableCollection<MenuItem>(MenuItems.OrderBy(i => i.FoodName));
+                }
+                else
+                {
+                    MenuItems = new ObservableCollection<MenuItem>(MenuItems.OrderByDescending(i => i.FoodName));
+                }
+            });
+
             LoadMenuItems();
             LoadOrderItems();
+            LoadComboBoxItems_2();
         }
 
         #region variable
             private ObservableCollection<MenuItem> selectedItems = new ObservableCollection<MenuItem>();
             private ObservableCollection<MenuItem> menuItems = new ObservableCollection<MenuItem>();
+            private ObservableCollection<string> comboboxItems_2 = new ObservableCollection<string>();
             public ObservableCollection<MenuItem> MenuItems
             {
                 get { return menuItems; }
-                set { menuItems = value; }
+                set {
+                    if (value != menuItems)
+                    {
+                        menuItems = value;
+                        OnPropertyChanged();
+                    }
+                }
             }
             public ObservableCollection<MenuItem> SelectedItems 
             {
                 get { return selectedItems; }
                 set { selectedItems = value;  OnPropertyChanged(); }
             }
+            public ObservableCollection<string> ComboboxItems_2 { get; set; }
+            private string myComboboxSelectedItem_2 = "A-Z";
+            public string MyComboboxSelectedItem_2
+        {
+                get { return myComboboxSelectedItem_2; }
+                set
+                {
+                    if (value != myComboboxSelectedItem_2)
+                    {
+                        myComboboxSelectedItem_2 = value;
+                        OnPropertyChanged();
+                    }
+                    
+                }
+            }
+
             private decimal dec_subtotal = 0;
             private string str_subtotal = "0 VND";
 
@@ -68,13 +138,16 @@ namespace QuanLyNhaHang.ViewModel
                 }
             }
         #endregion
-            
-
 
         #region commands
             public ICommand DirectingFoodToOrderListCommand { get; set; }
             public ICommand RemovingFoodFromOrderListCommand { get; set; }
-            public ICommand FilterFoodCommand { get; set; }
+            public ICommand RemovingAllItemFromOrderList { get; set; }
+            public ICommand FilterFoodByPriceCommand { get; set; }
+            public ICommand ShowPrintingDialog { get; set; }
+            public ICommand FoodCookingInform { get; set; }
+            public ICommand OpeningDetailOrder { get; set; }   
+
 
         #endregion
        
@@ -89,6 +162,10 @@ namespace QuanLyNhaHang.ViewModel
             menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 7, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
             menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 8, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
             menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            menuItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
 
             MenuItems = menuItems;
         }
@@ -98,7 +175,14 @@ namespace QuanLyNhaHang.ViewModel
             
             SelectedItems = selectedItems;
 
-            //SelectedItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+            SelectedItems.Add(new MenuItem { FoodImage = "pack://application:,,,/images/menuitem_6.jpg", ID = 9, FoodName = "Cá Ngừ Đại Dương", Price = 1000000 });
+        }
+        public void LoadComboBoxItems_2()
+        {
+            comboboxItems_2.Add("A-Z");
+            comboboxItems_2.Add("Z-A");
+            comboboxItems_2.Add("DESC Price");
+            ComboboxItems_2 = comboboxItems_2;
         }
 
         public void OrderAnItem(int ID)
@@ -110,20 +194,17 @@ namespace QuanLyNhaHang.ViewModel
                 {
                     DecSubtotal += MenuItems[i].Price;
                     Subtotal = String.Format("{0:0,0 VND}", DecSubtotal);
+                    MenuItem x = CheckIfSelectedItemsContain(MenuItems[i]);
+                    if (x != null)
+                    {
+                        //get item through ID then increase the item's quantity by 1 unit.
+                        x.Quantity += 1;
+                        return;
+                    }
                     SelectedItems.Add(MenuItems[i]);
                 }
             } 
         }
-
-        //public string CalculatingSubtotal()
-        //{
-        //    decimal total = 0;
-        //    for(int i = 0; i < SelectedItems.Count; i++)
-        //    {
-        //        total += SelectedItems[i].Price;
-        //    }
-        //    return String.Format("{0:0,0 VND}", total);
-        //}
 
         public void RemoveAnItem(int ID)
         {
@@ -133,9 +214,34 @@ namespace QuanLyNhaHang.ViewModel
                 {
                     DecSubtotal -= SelectedItems[i].Price;
                     Subtotal = String.Format("{0:0,0 VND}", DecSubtotal);
-                    SelectedItems.RemoveAt(i);
+
+                    if(SelectedItems[i].Quantity <= 1)
+                    {
+                        SelectedItems.RemoveAt(i);
+                    }
+                    else
+                    {
+                        SelectedItems[i].Quantity -= 1;
+                    }
                 }
             }
+        }
+
+        public void RemoveAllItemOL()
+        {
+            SelectedItems.Clear();
+            DecSubtotal = 0;
+        }
+
+        //OUTER FUNCTION
+        public MenuItem CheckIfSelectedItemsContain(MenuItem x)
+        {
+            for(int i = 0; i < SelectedItems.Count; i++)
+            {
+                if (x.ID == SelectedItems[i].ID)
+                    return SelectedItems[i];
+            }
+            return null;
         }
     }
 }
